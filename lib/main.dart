1bf1af404189dd1ac57dc0a1090574bed45e9d35
wandermoschetta <wander.sm@gmail.com>
2020-06-11 16:24:00 -0300

    criado componente de estado

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,58 @@ import 'package:flutter/material.dart';
 
 main() => runApp(PerguntaApp());
 
+class _PerguntaAppState extends State<PerguntaApp> {
+  var _perguntaSelecionada = 0;
 
-class PerguntaApp extends StatelessWidget {
+  void _responder(){
+    setState(() {
+      _perguntaSelecionada++;  
+    });
+    
+    print(_perguntaSelecionada);
+  }
+  
+  @override
   Widget build(BuildContext context){
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
+    
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas') ,
         ),
-        body: Text('Olá flutter!!!'),
+        body: Column(
+          children: <Widget>[
+            Text(perguntas[_perguntaSelecionada]),
+            RaisedButton(
+              child: Text('Resposta 1'),
+              onPressed: _responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 2'),
+              onPressed: _responder,
+            ),
+            RaisedButton(
+              child: Text('Resposta 3'),
+              onPressed: _responder,
+            ),
+          ],
+
+        ),
       ),
     );
   }
+
+}
+
+class PerguntaApp extends StatefulWidget {
+  
+  _PerguntaAppState createState() {
+    return _PerguntaAppState();
+  }
+
+  
 }
