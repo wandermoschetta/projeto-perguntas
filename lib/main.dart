f11946c8f773e4d8a92bf6ef4275ce64f5feb247
wandermoschetta <wander.sm@gmail.com>
2020-06-11 21:24:11 -0300

    otimizado respostas

--- lib/main.dart
+++ lib/main.dart
@@ -18,10 +18,26 @@ class _PerguntaAppState extends State<PerguntaApp> {
   @override
   Widget build(BuildContext context){
     final perguntas = [
-      'Qual é a sua cor favorita?',
-      'Qual é o seu animal favorito?',
+      {
+        'texto': 'Qual é a sua cor favorita?',
+        'respostas':['Preto','Vermelho','Verde','Branco'],
+      },
+      {
+        'texto': 'Qual é o seu animal favorito?',
+        'respostas': ['Coelho','Cobra','Elefante','Leão'],
+      },
+      {
+        'texto': 'Qual o seu instrutor favorito?',
+        'respostas': ['Maria','João','Léo','Pedro'],
+      }
     ];
     
+    List<Widget> respostas = [];
+
+    for(String textoResp in perguntas[_perguntaSelecionada]['respostas']){
+      respostas.add(Resposta(textoResp, _responder));
+    }
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
@@ -29,10 +45,8 @@ class _PerguntaAppState extends State<PerguntaApp> {
         ),
         body: Column(
           children: <Widget>[
-            Questao(perguntas[_perguntaSelecionada]),
-            Resposta('Resposta 1', _responder),
-            Resposta('Resposta 2', _responder),
-            Resposta('Resposta 3', _responder),
+            Questao(perguntas[_perguntaSelecionada] ['texto']),
+            ...respostas,
           
           ],
 
